1f8ba3a2cba40177a19fb27b040067315c87a896
Cadu Olivera <caduolivera@Cadus-MacBook-Pro.local>
2020-12-28 18:40:17 -0300

    feat: removing unnecessary files

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,22 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { authSagas } from './auth';
 import { patientSagas } from './patient';
-import { ubsSagas } from './ubs';
-import { preDiseaseSagas } from './preDisease';
-import { allergySagas } from './allergy';
-import { addressSagas } from './address';
-import { symptomSagas } from './symptom';
-import { selfEvaluationSagas } from './selfEvaluation';
 
 export default function* rootSaga() {
-  yield all([
-    ...authSagas,
-    ...patientSagas,
-    ...preDiseaseSagas,
-    ...allergySagas,
-    ...ubsSagas,
-    ...addressSagas,
-    ...symptomSagas,
-    ...selfEvaluationSagas,
-  ]);
+  yield all([...authSagas, ...patientSagas]);
 }
